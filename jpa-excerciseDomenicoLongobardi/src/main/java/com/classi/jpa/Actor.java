2fdae118178710d0668e413400d5a92075fcbc41
MimmoL <kynzomail@gmail.com>
2022-06-22 14:15:51 +0200

    Aggiunte relazioni ManyToMany tra actir e movie

--- jpa-excerciseDomenicoLongobardi/src/main/java/com/classi/jpa/Actor.java
+++ jpa-excerciseDomenicoLongobardi/src/main/java/com/classi/jpa/Actor.java
@@ -1,13 +1,16 @@
 package com.classi.jpa;
 
 import java.io.Serializable;
+
 import java.util.Set;
 
 import javax.persistence.Column;
+import javax.persistence.JoinColumn;
 import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
+import javax.persistence.JoinTable;
 import javax.persistence.ManyToMany;
 import javax.persistence.Table;
 
@@ -30,7 +33,12 @@ public class Actor implements Serializable {
 	private int birthdateYear;
 	
 	@ManyToMany
-	Set<Movie> actedMovies;
+	@JoinTable(
+			name = "actorsInTheMovie",
+			joinColumns = @JoinColumn(name = "actor_id"),
+			inverseJoinColumns = @JoinColumn(name = "movie_id")
+			)
+	Set<Movie> actedInMovies;
 
 
 	public int getId() {
--- jpa-excerciseDomenicoLongobardi/src/main/java/com/classi/jpa/Movie.java
+++ jpa-excerciseDomenicoLongobardi/src/main/java/com/classi/jpa/Movie.java
@@ -8,6 +8,8 @@ import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
+import javax.persistence.JoinColumn;
+import javax.persistence.JoinTable;
 import javax.persistence.ManyToMany;
 import javax.persistence.Table;
 
@@ -32,6 +34,11 @@ public class Movie implements Serializable{
 	private int genreId;
 	
 	@ManyToMany
+	@JoinTable(
+			name = "actedInMovies",
+			joinColumns = @JoinColumn(name = "movie_id"),
+			inverseJoinColumns = @JoinColumn(name = "actor_id")
+			)
 	Set<Actor> actorsInTheMovie;
 	
 	
