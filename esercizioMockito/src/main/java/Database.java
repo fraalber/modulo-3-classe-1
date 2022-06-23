8ad8bb5dab4374fe2614bd7efe7e7b7cbd9941f7
Canalella <mario.canalella-cic [at] ibm [dot] com>
2022-06-23 12:25:52 +0200

    update esercizio 2 junit

--- esercizioMockito/src/main/java/Database.java
+++ esercizioMockito/src/main/java/Database.java
@@ -3,7 +3,12 @@ public class Database {
     public boolean isAvailable() {
         return false;
     }
+    
     public int getUniqueId() {
         return 42;
+    }
+
+    public void setUniqueId(int value) {
+        return;
     }
 }
